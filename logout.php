7351d8c1ed32edd4313d5fc37f161995d10974ca
Mukesh Kr singh <mukesh.puppy@gmail.com>
2018-11-23 15:34:00 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -12,10 +12,9 @@ if (!isset($_SESSION['user'])) {
 }
 
 if (isset($_GET['logout'])) {
-	//$inactive='inactive';
-	echo "Mukesh";
-		$sql1="UPDATE `users` SET `status` = 'inactive' WHERE `users`.`id` = $_SESSION['user']";
-		$res1=mysqli_query($conn, $sql1);
+	$id = $_SESSION['user'];
+	$sql1="UPDATE `users` SET `status` = 'inactive' WHERE `users`.`id` = $id";
+	$res1=mysqli_query($conn, $sql1);
     session_destroy();
     unset($_SESSION['user']);
 	unset($_SESSION['user_type']);
